c9a872358575b69a399dfefe3b7accb5d17a96bc
Taylor Thompson <taylor@nightshiftwaxco.com>
2023-01-28 21:02:20 -0500

    testing deploying to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://einein07.github.io',
 });
